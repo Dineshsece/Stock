597a95f8b412a19b01e7066e17cbec8fd18d5e30
Elango <elango.t2022cce@sece.ac.in>
2024-04-25 14:03:41 +0530

    styke added

--- src/Components/Brokers.jsx
+++ src/Components/Brokers.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import './brokers.css';
 
 function Brokers(){
   return (
--- src/Components/Guide.jsx
+++ src/Components/Guide.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import './Guide.css'
 
 function Guide(){
     return(
